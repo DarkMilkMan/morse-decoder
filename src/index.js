cbd3283b781e731ab18e699e983e00fddf5cde07
Dmitry <darkmilkman@gmail.com>
2023-02-27 14:43:18 +0300

    feat: success

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arr2 = [];
+    let result = '';
+    arr.push(expr.match(/.{1,10}/g))
+    arr[0].forEach(el => {
+        let morseStr = '';
+        for (let i=0;i<10;i+=2){
+            if(el[i]+el[i+1] == '10'){
+                morseStr += '.'
+            } else if (el[i]+el[i+1] == '11'){
+                morseStr += '-'
+            } else if (el[i] == '*'){
+                arr2.push(' ');
+                i += 9;
+            }
+        }
+        arr2.push(morseStr);
+    })
+    arr2.forEach(el => {
+        if (el == ' '){
+            result += el;
+        }
+        for (key in MORSE_TABLE) {
+            if (el == key){
+                result += MORSE_TABLE[key];
+            }
+        }
+    })
+    return result;
 }
 
 module.exports = {
